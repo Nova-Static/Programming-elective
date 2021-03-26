55b62332a93aa15153e8720802be89a1db741631
Ramjar420 <61980781+Ramjar420@users.noreply.github.com>
2021-03-26 13:49:00 +0100

    almost finished with my AI

--- Assets/Scripts/RamjarAI.cs
+++ Assets/Scripts/RamjarAI.cs
@@ -6,17 +6,20 @@ public class RamjarAI : BaseAI
 {
     private Dictionary<string, RadarBlibInfo> radar = new Dictionary<string, RadarBlibInfo>();
     
-    private Vector3 LatestEnemyPos;
-    bool enemyInView;
-    bool inCombat;
+    private Vector3 _flagPos;
+    private bool _closeToFlag = false;
     
-    private Vector3 FlagPosition;
-
-    private bool searchingForEnemy = false;
-    private bool nearFlag = false;
+    private Vector3 _lastEnemyPos;
     
+    private bool _inCombat = false;
+    private float _combatTimer;
+    private bool _enemyInVision = false;
+    private float _enemySpottedTimer;
+    private float _timeSinceEnemyFound = 99f;
     
+    float _lastHealth;
     
+    private bool _runOnStart = false;
     // Start is called before the first frame update
     public RamjarAI()
     {
@@ -25,16 +28,43 @@ public class RamjarAI : BaseAI
     // Update is called once per frame
     public override void Update()
     {
-        if (!enemyInView && !getCapturingState())
+        //check health (logic from SzilardAI)
+        if (_lastHealth != GetHealth())
         {
-            GoToFlag();
+            _lastHealth = GetHealth();
+            _combatTimer = Time.time;
+            _inCombat = true;
         }
+        //reset timer for spotting enemy
+        if (_timeSinceEnemyFound > 2) _enemyInVision = false;
+        
+        //reset combat state after 3 sec
+        if (_combatTimer > 2) _inCombat = false;
+        
+        // rotate to enemy when spotted
+        if (_enemyInVision) RotateTo(_lastEnemyPos - GetPosition());
+        
+        //rotate to enemy when getting attacked from blind spot
+        if (!_enemyInVision && _inCombat) RotateTo(_lastEnemyPos - GetPosition());
+        
+        // Get distance to Flag
+        if (Vector3.Distance(GetPosition(), _flagPos) < 5.0f) _closeToFlag = true;
+        
+        else _closeToFlag = false;
+        
+        //If the flag is not getting captured or not near it, go to flag pos.
+        if (!getCapturingState() || !_closeToFlag)
+        {
+            //go to flag when no enemy spotted.
+            if (!_enemyInVision) GoToFlag();
+            //stop mech from moving to flag when enemy spotted.
+            else RotateTo(_lastEnemyPos - GetPosition());
+        }
+        //(logic from SzilardAI)
+        _timeSinceEnemyFound = Time.time - _enemySpottedTimer;
     }
     public override void OnRecordRadarBlib(RadarBlibInfo info)
     {
-        Fire(info.transform);
-        LatestEnemyPos = info.transform.position;
-        
         if (radar.ContainsKey(info.name))
         {
             radar[info.name] = info;
@@ -43,10 +73,16 @@ public class RamjarAI : BaseAI
         {
             radar.Add(info.name, info);
         }
+        _enemyInVision = true;
+        //attack target
+        Fire(info.transform);
+        //get latest position of enemy
+        _lastEnemyPos = info.transform.position;
+        _enemySpottedTimer = Time.time;
     }
     public override void OnFlagInfo(FlagInfo info)
     {
-        FlagPosition = info.position;
+        _flagPos = info.position;
     }
     private void GoToFlag()
     {
